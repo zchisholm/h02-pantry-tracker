069fd053f69f70d095d0865b99ba8472509544f7
zchisholm <shakwanc21@gmail.com>
2024-08-11 17:58:26 -0400

    Updated page.js
    Updated page.js and added the modal for adding items

--- app/page.js
+++ app/page.js
@@ -1,34 +1,127 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { firestore } from '@/firebase';
-import { Box, Typography } from "@mui/material";
+import { firestore } from "@/firebase";
+import {
+  Box,
+  Modal,
+  TextField,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
+import { collection, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
-  const [Inventory, setInventory] = useState([])
-  const [open, setOpen] = userState([])
-  const [itemName, setItemName] = userState('')
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(true);
+  const [itemName, setItemName] = useState("");
 
-  const updateInventory = async 90 => {
-    const snapshot = query(collection(firestore, 'inventory'))
-    const docs = await getDocs(snapshot)
-    const inventoryList = []
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
     docs.forEach((doc) => {
       inventoryList.push({
         name: doc.id,
         ...doc.data(),
-      })
-    })
-    setInventory(inventoryList)
-  }
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(doref);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+
+    await updateInventory();
+  };
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(doref);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+    await updateInventory();
+  };
 
   useEffect(() => {
-    updateInventory()
-  }, [])
+    updateInventory();
+  }, []);
 
-  return(
-    <Box>
-      <Typography variant ="h1">Inventory Management</Typography>
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: 'translate(-50%,-50%")',
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName);
+                setItemName("");
+                handleClose();
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Typography variant="h1">Inventory Management</Typography>
+      {inventory.forEach((item) => {
+        console.log(item);
+        return (
+          <>
+            {item.name}
+            {item.quantity}
+          </>
+        );
+      })}
     </Box>
-  )
+  );
 }
